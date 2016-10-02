e1d703b08cf7ffbbb3aa98b010aba1aeb808fa24
Yasuyuki Ueta <yasuyuki.ueta.1216@gmail.com>
2016-10-02 10:15:01 +0000

    tutorial7

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name "Ueta Yasuyuki"
+        email "yasuyuki@example.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
